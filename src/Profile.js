a273d282f9d3e20299598c61be3e62d72fff4ce0
DrewHalverson <drew@navlys.com>
2023-04-22 20:05:47 -0500

    fixed deprecated code in Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Auth } from 'aws-amplify'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import Container from './Container'
 
 const Profile = () => {
@@ -17,13 +18,19 @@ const Profile = () => {
   }
   return (
     <Container>
-      <h1>Profile</h1>
-      <h2>Username: {user.username}</h2>
-      <h3>Email: {user.email}</h3>
-      <h4>Phone: {user.phone_number}</h4>
-      <AmplifySignOut />
+      <AmplifyAuthenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Profile</h1>
+            <h2>Username: {user.username}</h2>
+            <h3>Email: {user.email}</h3>
+            <h4>Phone: {user.phone_number}</h4>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+        </AmplifyAuthenticator>
     </Container>
   );
 }
 
-export default withAuthenticator(Profile)
+export default Profile
